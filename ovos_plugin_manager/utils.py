b461c4eeb2738339e0d994a26be07b13c29165ab
jarbasal <jarbasai@mailfence.com>
2021-05-18 18:34:04 +0100

    intent plugins

--- ovos_plugin_manager/utils.py
+++ ovos_plugin_manager/utils.py
@@ -23,7 +23,7 @@ class PluginTypes(str, Enum):
     WAKEWORD = 'mycroft.plugin.wake_word'
     TRANSLATE = "neon.plugin.lang.translate"
     LANG_DETECT = "neon.plugin.lang.detect"
-    INTENT = "chatterbox.plugin.intents"
+    INTENT = "chatterbox.plugin.intentBox"
 
 
 def find_plugins(plug_type=None):
