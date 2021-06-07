97c3349b1631760bfecd69ff70efe2c45f4c0cc7
jarbasal <jarbasai@mailfence.com>
2021-06-07 20:16:45 +0100

    reqs

--- ovos_plugin_manager/__init__.py
+++ ovos_plugin_manager/__init__.py
@@ -1,12 +1,2 @@
-try:
-    import mycroft
-except ImportError:
-    from ovos_utils.system import search_mycroft_core_location
-    MYCROFT_ROOT = search_mycroft_core_location()
-    if MYCROFT_ROOT:
-        import sys
-        sys.path.append(MYCROFT_ROOT)
-    import mycroft
-
 from ovos_plugin_manager.utils import load_plugin, find_plugins, PluginTypes
 from ovos_plugin_manager.plugin_entry import MycroftPlugin
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ovos-plugin-manager',
-    version='0.0.1a2',
+    version='0.0.1a3',
     packages=['ovos_plugin_manager', 'ovos_plugin_manager.templates'],
     url='https://github.com/OpenVoiceOS/OVOS-plugin-manager',
     license='Apache-2.0',
