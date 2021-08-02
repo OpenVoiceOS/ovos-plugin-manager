ac0a1e5774217e0ce46ae6f3ae4612b4a556e570
jarbasal <jarbasai@mailfence.com>
2021-08-02 18:25:23 +0100

    bump alpha

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     url='https://github.com/OpenVoiceOS/OVOS-plugin-manager',
     license='Apache-2.0',
     author='jarbasAi',
-    install_requires=["ovos_utils>=0.0.11", "requests", "memory-tempfile",
+    install_requires=["ovos_utils>=0.0.12a1",
+                      "requests",
+                      "memory-tempfile",
                       "phoneme_guesser"],
     author_email='jarbasai@mailfence.com',
     description='OpenVoiceOS plugin manager'
