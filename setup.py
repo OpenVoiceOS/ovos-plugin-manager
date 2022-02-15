29a2233162333106eae465e811d5c8538088d538
NeonJarbas <59943014+NeonJarbas@users.noreply.github.com>
2022-02-15 17:00:35 +0000

    refactor/bump_requests (#28)
    bump min version to avoid GPL transient dependency (chardet)
    
    authored-by: jarbasai <jarbasai@mailfence.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='Apache-2.0',
     author='jarbasAi',
     install_requires=["ovos_utils>=0.0.14a6",
-                      "requests",
+                      "requests~=2.26",
                       "combo_lock~=0.2.1",
                       "memory-tempfile"],
     author_email='jarbasai@mailfence.com',
