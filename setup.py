7607f5bfbe29d4cb6fbccd525220ec01ed5ad49d
NeonJarbas <59943014+NeonJarbas@users.noreply.github.com>
2022-02-15 16:58:43 +0000

    fix/remove_pyxdg (#30)
    authored-by: jarbasai <jarbasai@mailfence.com>

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='ovos-plugin-manager',
-    version='0.0.3a5',
+    version='0.0.4a2',
     packages=['ovos_plugin_manager', 'ovos_plugin_manager.templates', 'ovos_plugin_manager.utils'],
     url='https://github.com/OpenVoiceOS/OVOS-plugin-manager',
     license='Apache-2.0',
     author='jarbasAi',
-    install_requires=["ovos_utils>=0.0.12a3",
+    install_requires=["ovos_utils>=0.0.14a6",
                       "requests",
+                      "combo_lock~=0.2.1",
                       "memory-tempfile"],
     author_email='jarbasai@mailfence.com',
     description='OpenVoiceOS plugin manager'
