4339989c5d7d4b31c0d7d6b7dafbd24a5a8116d0
jarbasai <jarbasai@mailfence.com>
2022-02-18 21:39:42 +0000

    feat/pypi_workflow

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+
+def _get_version():
+    with open('ovos_plugin_manager/versioning/opm_versions.py') as versions:
+        for line in versions:
+            if line.startswith('CURRENT_OPM_VERSION'):
+                # CURRENT_OSM_VERSION = "0.0.10a9" --> "0.0.10a9"
+                return line.replace('"','').strip('\n').split('= ')[1]
+
+
 setup(
     name='ovos-plugin-manager',
-    version='0.0.5',
+    version=_get_version(),
     packages=['ovos_plugin_manager', 'ovos_plugin_manager.templates', 'ovos_plugin_manager.utils'],
     url='https://github.com/OpenVoiceOS/OVOS-plugin-manager',
     license='Apache-2.0',
