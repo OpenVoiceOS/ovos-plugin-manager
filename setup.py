f8f2575789c57e6d5655678b0729fffe43341e06
jarbasai <jarbasai@mailfence.com>
2022-02-24 20:36:18 +0000

    feat/release_workflows

--- setup.py
+++ setup.py
@@ -1,18 +1,40 @@
+import os
 from setuptools import setup
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-def _get_version():
-    with open('ovos_plugin_manager/versioning/opm_versions.py') as versions:
-        for line in versions:
-            if line.startswith('CURRENT_OPM_VERSION'):
-                # CURRENT_OSM_VERSION = "0.0.10a9" --> "0.0.10a9"
-                return line.replace('"','').strip('\n').split('= ')[1]
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_plugin_manager', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
 
 
 setup(
     name='ovos-plugin-manager',
-    version=_get_version(),
-    packages=['ovos_plugin_manager', 'ovos_plugin_manager.templates', 'ovos_plugin_manager.utils'],
+    version=get_version(),
+    packages=['ovos_plugin_manager',
+              'ovos_plugin_manager.templates',
+              'ovos_plugin_manager.utils'],
     url='https://github.com/OpenVoiceOS/OVOS-plugin-manager',
     license='Apache-2.0',
     author='jarbasAi',
