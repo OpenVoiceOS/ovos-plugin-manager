14f7c1e9a98f1f3be9aa3abd52b8ed58394e731b
jarbasai <jarbasai@mailfence.com>
2022-03-03 16:23:14 +0000

    add ssml unittests

--- test/unittests/test_ssml.py
+++ test/unittests/test_ssml.py
@@ -1,9 +1,69 @@
 # write your first unittest!
 import unittest
 from ovos_plugin_manager.templates.tts import TTS
+from ovos_utils.messagebus import FakeBus
 
 
 class TestSSML(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        tts = TTS()  # dummy engine
+       # bus = FakeBus()
+       # tts.init(bus)
+        self.tts = tts
+
+    def test_ssml(self):
+        sentence = "<speak>Prosody can be used to change the way words " \
+                   "sound. The following words are " \
+                   "<prosody volume='x-loud'> " \
+                   "quite a bit louder than the rest of this passage. " \
+                   "</prosody> Each morning when I wake up, " \
+                   "<prosody rate='x-slow'>I speak quite slowly and " \
+                   "deliberately until I have my coffee.</prosody> I can " \
+                   "also change the pitch of my voice using prosody. " \
+                   "Do you like <prosody pitch='+5%'> speech with a pitch " \
+                   "that is higher, </prosody> or <prosody pitch='-10%'> " \
+                   "is a lower pitch preferable?</prosody></speak>"
+        sentence_no_ssml = "Prosody can be used to change the way " \
+                           "words sound. The following words are quite " \
+                           "a bit louder than the rest of this passage. " \
+                           "Each morning when I wake up, I speak quite " \
+                           "slowly and deliberately until I have my " \
+                           "coffee. I can also change the pitch of my " \
+                           "voice using prosody. Do you like speech " \
+                           "with a pitch that is higher, or is " \
+                           "a lower pitch preferable?"
+        sentence_bad_ssml = "<foo_invalid>" + sentence + \
+                            "</foo_invalid end=whatever>"
+        sentence_extra_ssml = "<whispered>whisper tts<\\whispered>"
+
+        tts = TTS()  # dummy engine
+        # test valid ssml
+        tts.ssml_tags = ['speak', 'prosody']
+        self.assertEqual(tts.validate_ssml(sentence), sentence)
+
+        # test extra ssml
+        tts.ssml_tags = ['whispered']
+        self.assertEqual(tts.validate_ssml(sentence_extra_ssml),
+                         sentence_extra_ssml)
+
+        # test unsupported extra ssml
+        tts.ssml_tags = ['speak', 'prosody']
+        self.assertEqual(tts.validate_ssml(sentence_extra_ssml),
+                         "whisper tts")
+
+        # test mixed valid / invalid ssml
+        tts.ssml_tags = ['speak', 'prosody']
+        self.assertEqual(tts.validate_ssml(sentence_bad_ssml), sentence)
+
+        # test unsupported ssml
+        tts.ssml_tags = []
+        self.assertEqual(tts.validate_ssml(sentence), sentence_no_ssml)
+
+        self.assertEqual(tts.validate_ssml(sentence_bad_ssml),
+                         sentence_no_ssml)
+
+        self.assertEqual(TTS.remove_ssml(sentence), sentence_no_ssml)
 
     def test_format_speak_tags_with_speech(self):
         valid_output = "<speak>Speak This.</speak>"
