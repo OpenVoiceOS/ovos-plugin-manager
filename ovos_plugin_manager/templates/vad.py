0d25fe141cbdf6f050bf70ed51094d7b442c4f8c
jarbasai <jarbasai@mailfence.com>
2022-03-05 11:34:03 +0000

    :tada:

--- ovos_plugin_manager/templates/vad.py
+++ ovos_plugin_manager/templates/vad.py
@@ -2,12 +2,14 @@ from ovos_utils.configuration import read_mycroft_config
 
 
 class VADEngine:
-    def __init__(self, config=None):
+    def __init__(self, config=None, sample_rate=None):
         try:
             self.config_core = read_mycroft_config() or {}
         except FileNotFoundError:
             self.config_core = {}
         self._init_config(config)
+        self.sample_rate = sample_rate or \
+                           self.config_core.get("listener", {}).get("sample_rate", 16000)
 
     def _init_config(self, config=None):
         if config is None:
@@ -16,7 +18,7 @@ class VADEngine:
         else:
             self.config = config
 
-    def is_silence(self, chunk, vad_threshold):
+    def is_silence(self, chunk):
         # return True or False
         return False
 
