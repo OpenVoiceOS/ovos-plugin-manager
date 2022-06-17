0277c90a50f1bd793be6728102366860f01ca424
NeonJarbas <59943014+NeonJarbas@users.noreply.github.com>
2022-06-17 17:57:59 +0100

    feat/keyword_xtract_plugins (#65)
    * feat/keyword_xtract_plugins
    
    * fix return type
    
    * remove lang assumptions
    
    Co-authored-by: jarbasai <jarbasai@mailfence.com>

--- ovos_plugin_manager/utils/__init__.py
+++ ovos_plugin_manager/utils/__init__.py
@@ -32,6 +32,7 @@ class PluginTypes(str, Enum):
     AUDIO_TRANSFORMER = "neon.plugin.audio"
     QUESTION_SOLVER = "neon.plugin.solver"
     COREFERENCE_SOLVER = "intentbox.coreference"
+    KEYWORD_EXTRACTION = "intentbox.keywords"
     UTTERANCE_SEGMENTATION = "intentbox.segmentation"
     TOKENIZATION = "intentbox.tokenization"
     POSTAG = "intentbox.postag"
