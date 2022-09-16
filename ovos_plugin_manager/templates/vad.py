ed94aca8f25d2369b39569707ab212d81f06f5b4
Daniel McKnight <daniel@neon.ai>
2022-09-16 16:39:10 -0700

    Update VAD config to match other plugins Resolve circular import as found https://github.com/NeonGeckoCom/neon_speech/actions/runs/3071305587/jobs/4961864143#step:5:37

--- ovos_plugin_manager/templates/vad.py
+++ ovos_plugin_manager/templates/vad.py
@@ -1,11 +1,10 @@
 from ovos_config import Configuration
-from ovos_plugin_manager.vad import get_vad_config
 
 
 class VADEngine:
     def __init__(self, config=None, sample_rate=None):
         self.config_core = Configuration()
-        config = config or get_vad_config()
+        self.config = config or {}
         self.sample_rate = sample_rate or \
                            self.config_core.get("listener", {}).get("sample_rate", 16000)
 
