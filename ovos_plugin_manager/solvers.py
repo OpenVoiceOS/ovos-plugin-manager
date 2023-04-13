29cfef7e37342817c8257a3f09f7c831a6da436e
JarbasAi <jarbasai@mailfence.com>
2023-04-13 22:05:45 +0100

    dont move service, just template

--- ovos_plugin_manager/solvers.py
+++ ovos_plugin_manager/solvers.py
@@ -1,7 +1,3 @@
-from ovos_config import Configuration
-from ovos_utils.log import LOG
-from ovos_utils.messagebus import FakeBus
-
 from ovos_plugin_manager.utils import load_plugin, normalize_lang, find_plugins, PluginTypes, PluginConfigTypes
 
 
@@ -60,45 +56,3 @@ def load_question_solver_plugin(module_name):
     """
     return load_plugin(module_name, PluginTypes.QUESTION_SOLVER)
 
-
-class QuestionSolversService:
-    def __init__(self, bus=None, config=None):
-        self.config_core = config or Configuration()
-        self.loaded_modules = {}
-        self.bus = bus or FakeBus()
-        self.config = self.config_core.get("solvers") or {}
-        self.load_plugins()
-
-    def load_plugins(self):
-        for plug_name, plug in find_question_solver_plugins().items():
-            config = self.config.get(plug_name) or {}
-            if not config.get("enabled", True):
-                LOG.debug(f"{plug_name} not enabled in config, it won't be loaded")
-                continue
-            try:
-                self.loaded_modules[plug_name] = plug(config=config)
-                LOG.info(f"loaded question solver plugin: {plug_name}")
-            except Exception as e:
-                LOG.exception(f"Failed to load question solver plugin: {plug_name}")
-
-    @property
-    def modules(self):
-        return sorted(self.loaded_modules.values(),
-                      key=lambda k: k.priority)
-
-    def shutdown(self):
-        for module in self.modules:
-            try:
-                module.shutdown()
-            except:
-                pass
-
-    def spoken_answer(self, utterance, context=None):
-        for module in self.modules:
-            try:
-                ans = module.spoken_answer(utterance, context)
-                if ans:
-                    return ans
-            except Exception as e:
-                LOG.error(e)
-                pass
