5bf4a3cf146ffd61323541b342c6021a3b0d4db4
JarbasAi <jarbasai@mailfence.com>
2023-04-22 11:21:10 +0100

    intentBox

--- ovos_plugin_manager/intents.py
+++ ovos_plugin_manager/intents.py
@@ -32,3 +32,53 @@ def load_intent_engine(module_name, config=None):
     else:
         LOG.error(f"intent plugin not found: {module_name}")
 
+
+class IntentBox(IntentExtractor):
+    """ loads all intent plugins, core interfaces with this class only """
+
+    def __init__(self, config=None,
+                 strategy=IntentDeterminationStrategy.SEGMENT_REMAINDER,
+                 priority=IntentPriority.LOW,
+                 segmenter=None):
+        super().__init__(config, strategy, priority, segmenter)
+        self.services = []
+        self._load_intent_plugins()
+
+    def _load_intent_plugins(self):
+        engines = []
+
+        for engine_id, engine_class in find_intent_plugins().items():
+            engine_config = self.config.get(engine_id, {})
+            if engine_config.get("disabled"):
+                continue
+            try:
+                plugin = engine_class(engine_config)
+                plugin.engine_id = engine_id
+                engines.append(plugin)
+            except Exception as e:
+                LOG.exception(f"Failed to load intent plugin: {engine_id}")
+
+        engines.sort(key=lambda k: k.priority)
+        self.services = engines
+        for e in engines:
+            LOG.info(f"Loaded intent engine: {e} with priority: {e.priority}")
+
+    def calc_intent(self, utterance, min_conf=0.0, lang=None, session=None):
+        lang = lang or self.lang
+        matches = []
+        for engine in self.services:  # sorted by engine priority
+            conf_modifier = max(100 - engine.priority, 1)  # engine weight from 0 - 100
+            LOG.info(f"Matching {utterance} with {engine}")
+            try:
+                for match in engine.calc(utterance, lang=lang, session=session):
+                    match.confidence = match.confidence * (conf_modifier / 100)
+                    matches.append(match)
+                    LOG.debug(f"intent candidate: {match}")
+            except:
+                LOG.exception(f"{engine} error!")
+
+        if not matches:
+            return None
+        matches = sorted(matches, key=lambda k: k.priority, reverse=True)
+        LOG.debug(f"best match: {matches[0]}")
+        return matches[0]
