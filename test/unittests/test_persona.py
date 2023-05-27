b1150e4c555f0a5970847cfb289377ca473bbc20
JarbasAi <jarbasai@mailfence.com>
2023-05-27 18:32:45 +0100

    personas are simple dicts

--- test/unittests/test_persona.py
+++ test/unittests/test_persona.py
@@ -1,18 +1,11 @@
 import unittest
-
 from unittest.mock import patch
-from ovos_plugin_manager.utils import PluginTypes, PluginConfigTypes
 
-
-class TestPersonaTemplate(unittest.TestCase):
-    def test_persona(self):
-        from ovos_plugin_manager.persona import Persona
-        # TODO
+from ovos_plugin_manager.utils import PluginTypes
 
 
 class TestPersona(unittest.TestCase):
     PLUGIN_TYPE = PluginTypes.PERSONA
-    CONFIG_TYPE = PluginConfigTypes.PERSONA
     TEST_CONFIG = {"test": True}
     CONFIG_SECTION = "persona"
     TEST_LANG = "en-us"
@@ -22,42 +15,3 @@ class TestPersona(unittest.TestCase):
         from ovos_plugin_manager.persona import find_persona_plugins
         find_persona_plugins()
         find_plugins.assert_called_once_with(self.PLUGIN_TYPE)
-
-    @patch("ovos_plugin_manager.utils.load_plugin")
-    def test_load_plugin(self, load_plugin):
-        from ovos_plugin_manager.persona import load_persona_plugin
-        load_persona_plugin("test_mod")
-        load_plugin.assert_called_once_with("test_mod", self.PLUGIN_TYPE)
-
-    @patch("ovos_plugin_manager.utils.config.load_configs_for_plugin_type")
-    def test_get_configs(self, load_configs):
-        from ovos_plugin_manager.persona import get_persona_configs
-        get_persona_configs()
-        load_configs.assert_called_once_with(self.PLUGIN_TYPE)
-
-    @patch("ovos_plugin_manager.utils.config.load_plugin_configs")
-    def test_get_module_configs(self, load_plugin_configs):
-        from ovos_plugin_manager.persona import get_persona_module_configs
-        get_persona_module_configs("test_mod")
-        load_plugin_configs.assert_called_once_with("test_mod",
-                                                    self.CONFIG_TYPE, True)
-
-    @patch("ovos_plugin_manager.utils.config.get_plugin_language_configs")
-    def test_get_lang_configs(self, get_language_configs):
-        from ovos_plugin_manager.persona import get_persona_lang_configs
-        get_persona_lang_configs(self.TEST_LANG)
-        get_language_configs.assert_called_once_with(self.PLUGIN_TYPE,
-                                                     self.TEST_LANG, False)
-
-    @patch("ovos_plugin_manager.utils.config.get_plugin_supported_languages")
-    def test_get_supported_langs(self, get_supported_languages):
-        from ovos_plugin_manager.persona import get_persona_supported_langs
-        get_persona_supported_langs()
-        get_supported_languages.assert_called_once_with(self.PLUGIN_TYPE)
-
-    @patch("ovos_plugin_manager.utils.config.get_plugin_config")
-    def test_get_config(self, get_config):
-        from ovos_plugin_manager.persona import get_persona_config
-        get_persona_config(self.TEST_CONFIG)
-        get_config.assert_called_once_with(self.TEST_CONFIG,
-                                           self.CONFIG_SECTION)
