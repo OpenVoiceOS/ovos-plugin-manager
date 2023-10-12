865ceba4c680cd101cd61bccde9e88592fbe51db
Daniel McKnight <daniel@neon.ai>
2023-10-12 14:15:52 -0700

    Fix test failures

--- test/integration/test_plugin_init.py
+++ test/integration/test_plugin_init.py
@@ -1,51 +1,23 @@
 import unittest
-import importlib
-import ovos_plugin_manager.tts
 
 from os import environ
 from os.path import join, dirname, isdir, isfile
 from shutil import rmtree
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 
-environ["OVOS_DEFAULT_LOG_NAME"] = "test"
 environ["OVOS_DEFAULT_LOG_LEVEL"] = "DEBUG"
 
 
 class TestPluginInit(unittest.TestCase):
-    # @patch("ovos_utils.log.LOG")
-    # def test_init_logging(self, log):
-    #     from ovos_plugin_manager.tts import load_tts_plugin
-    #     plugin = load_tts_plugin("ovos-tts-plugin-espeakng")
-    #     log.reset_mock()
-    #     tts = plugin()
-    #     log.debug.assert_any_call(f"Amplitude: None")
-    #     log.debug.assert_any_call(tts.config)
-    #     tts.shutdown()
-
-    # def test_log_method_called(self):
-    #     from ovos_utils.log import LOG
-    #     from ovos_plugin_manager.tts import load_tts_plugin
-    #     real_debug = LOG.debug
-    #     LOG.debug = Mock()
-    #
-    #     plugin = load_tts_plugin("ovos-tts-plugin-espeakng")
-    #     tts = plugin()
-    #     LOG.info(LOG.debug.call_args_list)
-    #     LOG.debug.assert_any_call(f"Amplitude: None")
-    #     LOG.debug.assert_any_call(tts.config)
-    #     tts.shutdown()
-    #
-    #     LOG.debug = real_debug
-
     @patch("mycroft.Configuration")
     def test_log_output(self, config):
-        from ovos_utils.log import LOG
-        from ovos_plugin_manager.tts import load_tts_plugin
-
         # Init log config
         test_log_dir = join(dirname(__file__), "logs")
         test_log_level = "DEBUG"
-        environ["OVOS_DEFAULT_LOG_NAME"] = "test"
+        test_log_name = "test"
+
+        from ovos_utils.log import LOG
+        from ovos_plugin_manager.tts import load_tts_plugin
 
         # Mock config for `mycroft` module init
         config.return_vaule = {
@@ -57,16 +29,21 @@ class TestPluginInit(unittest.TestCase):
                 "diagnostic": False
             }
         }
+
+        # This is basically what `init_service_logger` does but with test config
         LOG.init({"path": test_log_dir, "level": test_log_level})
+        LOG.name = test_log_name
 
         plugin = load_tts_plugin("ovos-tts-plugin-espeakng")
         self.assertEqual(LOG.base_path, test_log_dir, LOG.base_path)
         self.assertEqual(LOG.level, test_log_level, LOG.level)
+        self.assertEqual(LOG.name, test_log_name, LOG.name)
 
         tts = plugin()
 
         self.assertEqual(LOG.base_path, test_log_dir, LOG.base_path)
         self.assertEqual(LOG.level, test_log_level, LOG.level)
+        self.assertEqual(LOG.name, test_log_name, LOG.name)
         self.assertTrue(isdir(test_log_dir), test_log_dir)
         self.assertTrue(isfile(join(test_log_dir, "test.log")))
         with open(join(test_log_dir, "test.log"), 'r') as f:
