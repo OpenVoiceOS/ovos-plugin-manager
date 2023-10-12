a776150d6a0be4a7bab52c3d6e9ebfec79c46fb6
Daniel McKnight <daniel@neon.ai>
2023-10-12 11:38:07 -0700

    Add test case for "normal" log init and expected log outputs

--- test/integration/test_plugin_init.py
+++ test/integration/test_plugin_init.py
@@ -1,4 +1,7 @@
 import unittest
+
+from os.path import join, dirname, isdir, isfile
+from shutil import rmtree
 from unittest.mock import patch
 
 
@@ -11,3 +14,33 @@ class TestPluginInit(unittest.TestCase):
         tts = plugin()
         log.debug.assert_any_call(f"Amplitude: None")
         log.debug.assert_any_call(tts.config)
+
+    @patch("mycroft.Configuration")
+    def test_log_output(self, config):
+        from ovos_utils.log import LOG
+        from ovos_plugin_manager.tts import load_tts_plugin
+
+        # Init log config
+        test_log_dir = join(dirname(__file__), "logs")
+        test_log_level = "DEBUG"
+
+        # Mock config for `mycroft` module init
+        config.return_vaule = {
+            "log_level": test_log_level,
+            "logs": {
+                "path": test_log_dir
+            }
+        }
+        LOG.init({"path": test_log_dir, "level": test_log_level})
+
+        plugin = load_tts_plugin("ovos-tts-plugin-espeakng")
+        tts = plugin()
+        self.assertTrue(isdir(test_log_dir))
+        self.assertTrue(isfile(join(test_log_dir, "ovos.log")))
+        with open(join(test_log_dir, "ovos.log"), 'r') as f:
+            logs = f.read()
+        self.assertIn("Amplitude: ", logs)
+        self.assertIn(f"{tts.config}", logs)
+
+        # Cleanup
+        rmtree(test_log_dir)
