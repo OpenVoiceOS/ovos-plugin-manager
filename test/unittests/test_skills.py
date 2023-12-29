67b0b866597835cf808ec454a556e8a5a9e80494
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-12-29 13:58:14 +0000

    port unittests from workshop

--- test/unittests/test_skills.py
+++ test/unittests/test_skills.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 
 from ovos_plugin_manager.utils import PluginTypes, PluginConfigTypes
+from ovos_plugin_manager.skillz import get_skill_directories, get_default_skills_directory
 
 
 class TestSkills(unittest.TestCase):
@@ -16,6 +17,34 @@ class TestSkills(unittest.TestCase):
         find_skill_plugins()
         find_plugins.assert_called_once_with(self.PLUGIN_TYPE)
 
+    
+    def test_get_skill_directories(self):
+        # Default directory
+        mock_config = {'skills': {}}
+        default_directories = get_skill_directories(mock_config)
+        for directory in default_directories:
+            self.assertEqual(basename(directory), 'skills')
+        # Configured directory
+        mock_config['skills']['directory'] = 'test'
+        test_directories = get_skill_directories(mock_config)
+        for directory in test_directories:
+            self.assertEqual(basename(directory), 'test')
+        self.assertEqual(len(default_directories), len(test_directories))
+
+    def test_get_default_skills_directory(self):
+        # Default directory
+        mock_config = {'skills': {}}
+        default_dir = get_default_skills_directory(mock_config)
+        self.assertTrue(isdir(default_dir))
+        self.assertEqual(basename(default_dir), 'skills')
+        self.assertEqual(dirname(dirname(default_dir)), self.test_data_path)
+        # Override directory
+        mock_config['skills']['directory'] = 'test'
+        test_dir = get_default_skills_directory(mock_config)
+        self.assertTrue(isdir(test_dir))
+        self.assertEqual(basename(test_dir), 'test')
+        self.assertEqual(dirname(dirname(test_dir)), self.test_data_path)
+        
     def test_load_skill_plugins(self):
         from ovos_plugin_manager.skills import load_skill_plugins
         # TODO
