92108817372cfe9fae880c9b8cfa1498bf9b0a5c
miro <jarbasai@mailfence.com>
2024-10-16 07:10:41 +0100

    feat:pipeline plugin factory

--- ovos_plugin_manager/pipeline.py
+++ ovos_plugin_manager/pipeline.py
@@ -1,4 +1,9 @@
-from ovos_plugin_manager.templates.pipeline import PipelinePlugin
+from typing import List, Optional, Tuple, Callable, Union
+
+from ovos_bus_client.client import MessageBusClient
+from ovos_utils.fakebus import FakeBus
+from ovos_config import Configuration
+from ovos_plugin_manager.templates.pipeline import ConfidenceMatcherPipeline, PipelineStageMatcher, PipelinePlugin
 from ovos_plugin_manager.utils import PluginTypes
 
 
@@ -19,3 +24,122 @@ def load_pipeline_plugin(module_name: str) -> type(PipelinePlugin):
     """
     from ovos_plugin_manager.utils import load_plugin
     return load_plugin(module_name, PluginTypes.PIPELINE)
+
+
+class OVOSPipelineFactory:
+    _CACHE = {}
+
+    @staticmethod
+    def get_pipeline_classes(pipeline: Optional[List[str]] = None) -> List[Tuple[str, type(PipelinePlugin)]]:
+
+        default_p = [
+            "stop_high",
+            "converse",
+            "ocp_high",
+            "padatious_high",
+            "adapt_high",
+            "ocp_medium",
+            "fallback_high",
+            "stop_medium",
+            "adapt_medium",
+            "padatious_medium",
+            "adapt_low",
+            "common_qa",
+            "fallback_medium",
+            "fallback_low"
+        ]
+        pipeline = pipeline or Configuration().get("intents", {}).get("pipeline", default_p)
+
+        # TODO - deprecate around ovos-core 2.0.0
+        MAP = {
+            "converse": "ovos-converse-pipeline-plugin",
+            "common_qa": "ovos-common-query-pipeline-plugin",
+            "fallback_high": "ovos-fallback-pipeline-plugin-high",
+            "fallback_medium": "ovos-fallback-pipeline-plugin-medium",
+            "fallback_low": "ovos-fallback-pipeline-plugin-low",
+            "stop_high": "ovos-stop-pipeline-plugin-high",
+            "stop_medium": "ovos-stop-pipeline-plugin-medium",
+            "stop_low": "ovos-stop-pipeline-plugin-low",
+            "adapt_high": "ovos-adapt-pipeline-plugin-high",
+            "adapt_medium": "ovos-adapt-pipeline-plugin-medium",
+            "adapt_low": "ovos-adapt-pipeline-plugin-low",
+            "padacioso_high": "ovos-padacioso-pipeline-plugin-high",
+            "padacioso_medium": "ovos-padacioso-pipeline-plugin-medium",
+            "padacioso_low": "ovos-padacioso-pipeline-plugin-low",
+            "padatious_high": "ovos-padatious-pipeline-plugin-high",
+            "padatious_medium": "ovos-padatious-pipeline-plugin-medium",
+            "padatious_low": "ovos-padatious-pipeline-plugin-low",
+            "ocp_high": "ovos-ocp-pipeline-plugin-high",
+            "ocp_medium": "ovos-ocp-pipeline-plugin-medium",
+            "ocp_low": "ovos-ocp-pipeline-plugin-low"
+        }
+        valid_pipeline = [MAP.get(p, p) for p in pipeline]
+        matchers = []
+        for plug_id, clazz in find_pipeline_plugins().items():
+            if issubclass(clazz, ConfidenceMatcherPipeline):
+                if f"{plug_id}-low" in valid_pipeline:
+                    matchers.append((f"{plug_id}-low", clazz))
+                if f"{plug_id}-medium" in valid_pipeline:
+                    matchers.append((f"{plug_id}-medium", clazz))
+                if f"{plug_id}-high" in valid_pipeline:
+                    matchers.append((plug_id, clazz))
+            else:
+                matchers.append((plug_id, clazz))
+
+        return matchers
+
+    @staticmethod
+    def create(pipeline: Optional[List[str]] = None, use_cache: bool = True,
+               bus: Optional[Union[MessageBusClient, FakeBus]] = None) -> List[Tuple[str, Callable]]:
+        """Factory method to create pipeline matchers"""
+        default_p = [
+            "stop_high",
+            "converse",
+            "ocp_high",
+            "padatious_high",
+            "adapt_high",
+            "ocp_medium",
+            "fallback_high",
+            "stop_medium",
+            "adapt_medium",
+            "padatious_medium",
+            "adapt_low",
+            "common_qa",
+            "fallback_medium",
+            "fallback_low"
+        ]
+        pipeline = pipeline or Configuration().get("intents", {}).get("pipeline", default_p)
+
+        matchers = []
+        for pipe_id, clazz in OVOSPipelineFactory.get_pipeline_classes(pipeline):
+            if use_cache and pipe_id in OVOSPipelineFactory._CACHE:
+                m = OVOSPipelineFactory._CACHE[pipe_id]
+            else:
+                config = Configuration().get("intents", {}).get(pipe_id)
+                m = clazz(bus, config)
+                if use_cache:
+                    OVOSPipelineFactory._CACHE[pipe_id] = m
+            if isinstance(m, ConfidenceMatcherPipeline):
+                if pipe_id.endswith("-high"):
+                    matchers.append((pipe_id, m.match_high))
+                elif pipe_id.endswith("-medium"):
+                    matchers.append((pipe_id, m.match_medium))
+                elif pipe_id.endswith("-low"):
+                    matchers.append((pipe_id, m.match_low))
+            elif isinstance(m, PipelineStageMatcher):
+                matchers.append((pipe_id, m.match))
+        return matchers
+
+    @staticmethod
+    def shutdown():
+        for pipe in OVOSPipelineFactory._CACHE.values():
+            if hasattr(pipe, "shutdown"):
+                try:
+                    pipe.shutdown()
+                except:
+                    continue
+
+
+if __name__ == "__main__":
+    matchers = OVOSPipelineFactory.create()
+    print(matchers)
--- ovos_plugin_manager/templates/pipeline.py
+++ ovos_plugin_manager/templates/pipeline.py
@@ -1,18 +1,112 @@
-from collections import namedtuple
-from typing import Optional, Dict
+import abc
+from dataclasses import dataclass
+from typing import Optional, Dict, List, Union
 
-# Intent match response tuple, ovos-core expects PipelinePlugin to return this data structure
-# intent_service: Name of the service that matched the intent
-# intent_type: intent name (used to call intent handler over the message bus)
-# intent_data: data provided by the intent match
-# skill_id: the skill this handler belongs to
-IntentMatch = namedtuple('IntentMatch',
-                         ['intent_service', 'intent_type',
-                          'intent_data', 'skill_id', 'utterance']
-                         )
+from ovos_bus_client.message import Message
+from ovos_bus_client.client import MessageBusClient
+from ovos_utils.fakebus import FakeBus
+
+
+@dataclass()
+class IntentMatch:
+    # ovos-core expects PipelinePlugins to return this data structure
+    # replaces old IntentMatch response namedtuple
+    # intent_service: Name of the service that matched the intent
+    # intent_type: intent name (used to call intent handler over the message bus)
+    # intent_data: data provided by the intent match
+    # skill_id: the skill this handler belongs to
+    match_type: str
+    match_data: Optional[Dict] = None
+    skill_id: Optional[str] = None
+    utterance: Optional[str] = None
+
+
+@dataclass()
+class PipelineMatch(IntentMatch):
+    # same as above, but does not emit intent message on match
+    # the process of matching already handles the utterance
+    match_type: bool = True  # for compat only
+    handled: bool = True
+    match_data: Optional[Dict] = None
+    skill_id: Optional[str] = None
+    utterance: Optional[str] = None
 
 
 class PipelinePlugin:
-    """This class is a placeholder, this API will be defined in ovos-core release 0.1.0"""
     def __init__(self, config: Optional[Dict] = None):
         self.config = config or {}
+
+
+class ConfidenceMatcherPipeline(PipelinePlugin):
+    """these plugins return a match to the utterance,
+    but do not trigger an action directly during the match
+
+     eg. adapt, padatious"""
+
+    def __init__(self, bus: Optional[Union[MessageBusClient, FakeBus]] = None,
+                 config: Optional[Dict] = None):
+        self.bus = bus or FakeBus()
+        super().__init__(config=config)
+
+    @abc.abstractmethod
+    def match_high(self, utterances: List[str], lang: str, message: Message) -> Optional[IntentMatch]:
+        pass
+
+    @abc.abstractmethod
+    def match_medium(self, utterances: List[str], lang: str, message: Message) -> Optional[IntentMatch]:
+        pass
+
+    @abc.abstractmethod
+    def match_low(self, utterances: List[str], lang: str, message: Message) -> Optional[IntentMatch]:
+        pass
+
+
+class PipelineStageMatcher(PipelinePlugin):
+    """WARNING: has side effects when match is used
+
+    these plugins will consume an utterance during the match process,
+    aborting the next pipeline stages if a match is returned.
+
+    it is not known if this component will match without going through the match process
+
+    eg. converse, common_query """
+
+    def __init__(self, bus: Optional[Union[MessageBusClient, FakeBus]] = None,
+                 config: Optional[Dict] = None):
+        self.bus = bus or FakeBus()
+        super().__init__(config=config)
+
+    @abc.abstractmethod
+    def match(self, utterances: List[str], lang: str, message: Message) -> Optional[PipelineMatch]:
+        pass
+
+
+class PipelineStageConfidenceMatcher(PipelineStageMatcher, ConfidenceMatcherPipeline):
+    """WARNING: has side effects when match is used
+
+    these plugins will consume an utterance during the match process,
+    aborting the next pipeline stages if a match is returned.
+
+    it is not known if this component will match without going through the match process
+
+    eg. fallback, stop """
+
+    def __init__(self, bus: Optional[Union[MessageBusClient, FakeBus]] = None,
+                 config: Optional[Dict] = None):
+        super().__init__(bus=bus, config=config)
+
+    def match(self, utterances: List[str], lang: str, message: Message) -> Optional[PipelineMatch]:
+        # no match level specified, method still needs to be implmented since its a subclass of PipelineStageMatcher
+        return self.match_high(utterances, lang, message)
+
+    @abc.abstractmethod
+    def match_high(self, utterances: List[str], lang: str, message: Message) -> Optional[PipelineMatch]:
+        pass
+
+    @abc.abstractmethod
+    def match_medium(self, utterances: List[str], lang: str, message: Message) -> Optional[PipelineMatch]:
+        pass
+
+    @abc.abstractmethod
+    def match_low(self, utterances: List[str], lang: str, message: Message) -> Optional[PipelineMatch]:
+        pass
