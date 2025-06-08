058704fe9bc8ad985c23d1b55147555bd27b2417
miro <jarbasai@mailfence.com>
2025-06-08 20:43:29 +0100

    fix: wakeword factory
    missing update in 1.0.0 for new base class init signature

--- ovos_plugin_manager/wakewords.py
+++ ovos_plugin_manager/wakewords.py
@@ -3,7 +3,6 @@ import os
 from hashlib import md5
 from typing import Optional
 
-from ovos_utils.lang import standardize_lang_tag
 from ovos_utils.log import LOG
 from ovos_utils.xdg_utils import xdg_data_home
 
@@ -124,43 +123,36 @@ class OVOSWakeWordFactory:
         return load_wake_word_plugin(ww_module)
 
     @staticmethod
-    def load_module(module: str, hotword: str, hotword_config: dict,
-                    lang: str, loop=None) -> HotWordEngine:
+    def load_module(module: str, hotword: str, hotword_config: dict) -> HotWordEngine:
         """
         Get an initialized HotWordEngine using the specified module and hotword
         @param module: hotword plugin to load (not parsed)
         @param hotword: string hotword to load
         @param hotword_config: configuration for the specified `hotword`.
             Equivalent to Configuration()['hotwords'][hotword]
-        @param lang: BCP-47 language code of hotword
         @param loop: Unused
         @return: Initialized HotWordEngine
         """
-        lang = standardize_lang_tag(lang)
         # config here is config['hotwords'][module]
         LOG.info(f'Loading "{hotword}" wake word via {module} with '
                  f'config: {hotword_config}')
-        config = {"lang": lang, "hotwords": {hotword: hotword_config}}
+        config = {"hotwords": {hotword: hotword_config}}
         clazz = OVOSWakeWordFactory.get_class(hotword, config)
         if clazz is None:
             raise ImportError(f'Wake Word {hotword} with module {module} '
                               f'failed to load')
         LOG.info(f'Loaded the Wake Word {hotword} with module {module}')
-        return clazz(hotword, hotword_config, lang=lang)
+        return clazz(hotword, hotword_config)
 
     @classmethod
     def create_hotword(cls, hotword: str = "hey mycroft",
-                       config: Optional[dict] = None,
-                       lang: str = "en-US", loop=None) -> HotWordEngine:
+                       config: Optional[dict] = None) -> HotWordEngine:
         """
         Get an initialized HotWordEngine by configured name
         @param hotword: string hotword to load
         @param config: optional global configuration
-        @param lang: BCP-47 language code of hotword
-        @param loop: Unused
         @return: Initialized HotWordEngine
         """
-        lang = standardize_lang_tag(lang)
         ww_configs = get_hotwords_config(config)
         if hotword not in ww_configs:
             LOG.warning(f"replace ` ` in {hotword} with `_`")
@@ -168,12 +160,12 @@ class OVOSWakeWordFactory:
         ww_config = ww_configs.get(hotword)
         module = ww_config.get("module", "pocketsphinx")
         try:
-            return cls.load_module(module, hotword, ww_config, lang, loop)
+            return cls.load_module(module, hotword, ww_config)
         except Exception as e:
             LOG.error(f"Failed to load hotword: {hotword} - {module}")
             LOG.exception(e)
             fallback_ww = ww_config.get("fallback_ww")
             if fallback_ww in ww_configs and fallback_ww != hotword:
                 LOG.info(f"Attempting to load fallback ww instead: {fallback_ww}")
-                return cls.create_hotword(fallback_ww, config, lang, loop)
+                return cls.create_hotword(fallback_ww, config)
             raise
